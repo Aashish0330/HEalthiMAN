b53ea195a7c68896f47b53d36ad543171bd549a0
PrasannaKumaran <prasannakumaran176@gmail.com>
2021-11-14 15:33:14 +0530

    updated app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import uuid
 import json
 import requests
-from models import User,History
+from flask_login import UserMixin
 from pusher import Pusher
 from operator import methodcaller
 from flask_sqlalchemy import SQLAlchemy
@@ -37,6 +37,41 @@ def load_user(user_id):
 app.register_blueprint(auth)
 app.register_blueprint(main)
 db.create_all()
+
+class History(UserMixin, db.Model):
+   
+    __tablename__ = "history"
+    id = db.Column(db.Integer, primary_key =  True)
+    email = db.Column(db.String, db.ForeignKey("user.email"))
+    post_id = db.Column(db.Integer)
+    title = db.Column(db.String(50))
+    content = db.Column(db.String(300))
+    status = db.Column(db.String(10))
+    event_name = db.Column(db.String(20))
+    def __repr__(self):
+        return '<History %r>' % self.id
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    age = db.Column(db.Integer)
+    height = db.Column(db.Float)
+    weight = db.Column(db.Float)
+    country = db.Column(db.String(100))
+    bmi = db.Column(db.Float)
+    dob = db.Column(db.String(12))
+    gender = db.Column(db.String(20))
+    post = db.relationship('History', backref = db.backref('posts'))
+    email = db.Column(db.String(100), unique = True)
+    diet = db.Column(db.String(100))
+    calories = db.Column(db.Integer)
+    mealplan = db.Column(db.String(20000))
+    
+    def __repr__(self):
+        return '<User %r>' % self.email
 
 @auth.route('/login')
 def login():
